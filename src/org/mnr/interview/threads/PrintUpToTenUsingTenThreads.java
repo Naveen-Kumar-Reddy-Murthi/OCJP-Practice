68ed141c4d632efe10fe295a6328dd28a6716efb
naveen.reddy <mnreddy.nv@gmail.com>
2016-11-14 21:53:37 +0530

    fdfd

---
+++ src/org/mnr/interview/threads/PrintUpToTenUsingTenThreads.java
@@ -0,0 +1,43 @@
+package org.mnr.interview.threads;
+
+public class PrintUpToTenUsingTenThreads {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		PrintThread p = new PrintThread();
+		Thread t1= new Thread(p);
+		  t1.start();
+		  t1.join();
+		  Thread t2= new Thread(p);
+		  t2.start();
+		  t2.join();
+		  Thread t3= new Thread(p);
+		  t3.start();
+		  t3.join();
+		  Thread t4= new Thread(p);
+		  t4.start();
+		  t4.join();
+		  Thread t5= new Thread(p);
+		  t5.start();
+		  t5.join();
+		  Thread t6= new Thread(p);
+		  t6.start();
+		  t6.join();
+		  
+
+	}
+
+}
+
+class PrintThread implements Runnable{
+	
+	private int i;
+	
+
+	@Override
+	public void run() {
+		System.out.println(i++);
+		
+	}
+	
+}
---
+++ src/org/mnr/interview/threads/WaitNofifyDemo.java
@@ -0,0 +1,43 @@
+package org.mnr.interview.threads;
+
+public class WaitNofifyDemo {
+
+	public static void main(String[] args) {
+		MyRunnable run	=	new MyRunnable();
+		Thread t1 = new Thread(run);
+		t1.start();
+		synchronized (run) {
+			System.out.println("waiting");
+			try {
+				run.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("My waiting is over");
+			System.out.println("exiting...");
+		}
+
+	}
+
+}
+
+class MyRunnable implements Runnable{
+
+	@Override
+	public void run() {
+		synchronized (this) {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("I'm done. Nofifying");
+			this.notify();
+		}
+		
+	}
+	
+	
+}
